e16f41a21bd3705460663d54ce9a73d1c5f647a2
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-09 14:19:26 +0530

    create

--- 069nov/index.js
+++ 069nov/index.js
@@ -1,8 +1,24 @@
-const http=require("http");
-const fs=require("fs");
-http.createServer((req,res)=>{
-fs.readFile("my.txt",(err,data)=>{
-    res.write(data);
-    res.end();
-})
-}).listen(5000);
+// const http=require("http");
+// const fs=require("fs");
+// http.createServer((req,res)=>{
+// fs.readFile("my.txt",(err,data)=>{
+//     res.write(data);
+//     res.end();
+// })
+// }).listen(5000);
+
+
+
+const fs=require("fs")
+
+fs.appendFile("my.pdf","hello we are mern student",(err)=>{
+    if (err)throw err;
+    console.log("File Sussesfully creater!!!");
+
+} )
+
+fs.appendFile("our.txt","hello we are mern student",(err)=>{
+    if (err)throw err;
+    console.log("File Sussesfully creater!!!");
+
+} )
