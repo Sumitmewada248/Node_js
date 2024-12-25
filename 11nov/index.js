6680200792d55aa0d5fc3757dc605deb2c3b5745
Sumit mewada <sumitmewa@gmail.com>
2024-12-25 19:44:19 +0530

    Update index.js

--- 11nov/index.js
+++ 11nov/index.js
@@ -2,9 +2,9 @@ const fs=require("fs");
 
 
 // fs.open("ram.txt", "w", (err)=>{
-//     if(err) throw err;
+//   if(err) throw err;
 //     console.log("file sussesfull created !!!")
-// })
+//})
 
 
 fs.writeFile("ranu.txt", "welcome back", (err)=>{
